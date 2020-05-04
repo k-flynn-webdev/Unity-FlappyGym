260f16bedf33f82bd53aa4214db51f7bdbb809d4
kevin flynn <flynny85@gmail.com>
2020-05-04 12:40:26 +0100

    Fix kill repeated trigger bug

--- FlappyGym/Assets/Code/PlayerKill.cs
+++ FlappyGym/Assets/Code/PlayerKill.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerKill : MonoBehaviour
+public class PlayerKill : MonoBehaviour, IReset
 {
 
     [SerializeField]
     public bool _active = true;
+
+    private bool _isConsumed = false;
 
     [SerializeField]
     private float _force = 0f;
@@ -22,14 +24,25 @@ public class PlayerKill : MonoBehaviour
 
     void TriggerCol(Collider hitBy)
     {
+        if (_isConsumed)
+        {
+            return;
+        }
+
         if (_active && hitBy.CompareTag("Player"))
         {
             ServiceLocator.Resolve<Player>().Kill(_time);
+            _isConsumed = true;
 
             if (_force > 0f)
             {
                 ServiceLocator.Resolve<Player>().Force(_force, this.transform.position);
             }
         }
+    }
+
+    public void Reset()
+    {
+        _isConsumed = false;
     }
 }
