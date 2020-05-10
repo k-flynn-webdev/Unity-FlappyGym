bcc4bd1cfb83fd3be8672d2cdbded5980b1e8798
kevin flynn <flynny85@gmail.com>
2020-05-10 16:46:28 +0100

    Adding bg setup

---
+++ FlappyGym/Assets/Code/CameraBg.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBg : MonoBehaviour, ISubscribeState
+{
+
+    List<ObjectPoolItem> _clouds = new List<ObjectPoolItem>();
+
+    [SerializeField]
+    private Transform _screenLeft;
+    [SerializeField]
+    private Transform _screenRight;
+
+    private Vector3 _ScreenMid;
+
+    [SerializeField]
+    private int _cloudsMin = 10;
+    [SerializeField]
+    private int _cloudsMax = 10;
+
+    private bool _ready = false;
+    private float _checkTime = 0f;
+    private float _screenWidthDist = 0f;
+
+    [SerializeField]
+    public GameStateObj State { get; set; }
+
+
+    void Start()
+    {
+        ServiceLocator.Resolve<GameState>().SubscribeState(this);
+    }
+
+
+    void Update()
+    {
+        if (!_ready)
+        {
+            return;
+        }
+
+        if (Time.time > _checkTime)
+        {
+            CheckClouds();
+            _checkTime = Time.time + 0.5f;
+        }
+    }
+
+    private void Init()
+    {
+        _ScreenMid = Vector3.Lerp(_screenLeft.position, _screenRight.position, 0.5f);
+        _screenWidthDist = Vector3.Distance(_screenLeft.position, _screenRight.position) * 0.6f;
+
+        int cloudCount = Random.Range(_cloudsMin, _cloudsMax);
+
+        for (int i = 0; i < cloudCount; i++)
+        {
+            ObjectPoolItem tmp = ServiceLocator.Resolve<ObjectPoolManager>().GetItem("Cloud", true);
+            _clouds.Add(tmp);
+        }
+    }
+
+    private void ResetClouds()
+    {
+        for (int i = 0; i < _clouds.Count; i++)
+        {
+            ResetCloud(_clouds[i]);
+        }
+    }
+
+    private void ResetCloud(ObjectPoolItem cloud)
+    {
+        Vector3 newPos = Vector3.Lerp(_screenLeft.position, _screenRight.position, Random.Range(0f, 1f));
+        cloud.transform.position = newPos;
+        cloud.Reset();
+    }
+
+    private void CheckClouds()
+    {
+        float tmpDistance = 0f;
+
+        for (int i = 0, max = _clouds.Count; i < max; i++)
+        {
+            tmpDistance = Vector3.Distance(_ScreenMid, _clouds[i].transform.position);
+
+            if (tmpDistance > _screenWidthDist)
+            {
+                _clouds[i].transform.position = _screenLeft.position;
+                _clouds[i].Reset();
+            }
+        }
+    }
+
+    public void ReactState(GameStateObj state)
+    {
+        State = state;
+
+        if (!_ready)
+        {
+            Init();
+            ResetClouds();
+            _ready = true;
+        }
+    }
+}
---
+++ FlappyGym/Assets/Code/CloudBg.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBg : MonoBehaviour, IReset
+{
+
+    [SerializeField]
+    private float _cloudMoveSpeed = 1f;
+    [SerializeField]
+    private Vector3 _cloudDirection;
+    [SerializeField]
+    private float _heightRange = 2f;
+
+    private float _moveSpeed = 0f;
+
+
+
+    void Start()
+    {
+        InitCloud();
+    }
+
+    public void Reset()
+    {
+        InitCloud();
+    }
+
+    void Update()
+    {
+        UpdateCloud();
+    }
+
+    private void InitCloud()
+    {
+        _moveSpeed = Random.Range(_cloudMoveSpeed * 0.5f, _cloudMoveSpeed * 1.1f);
+        this.transform.localEulerAngles = new Vector3(Random.Range(0f, 180f), Random.Range(0f, 180f), Random.Range(0f, 180f));
+        float newScale = Random.Range(0.25f, 1.5f);
+        this.transform.localScale = Vector3.one * newScale;
+        Vector3 pos = this.transform.position;
+        pos.y += Random.Range(_heightRange * -1f, _heightRange * 1f);
+        pos.z += Random.Range(_heightRange * -1f, _heightRange * 1f);
+        this.transform.position = pos;
+    }
+
+    private void UpdateCloud()
+    {
+        this.transform.position += _cloudDirection.normalized * (Time.deltaTime * _moveSpeed);
+        Vector3 localRot = this.transform.localEulerAngles;
+        localRot.x += Time.deltaTime;
+        this.transform.localEulerAngles = localRot;
+
+    }
+}
