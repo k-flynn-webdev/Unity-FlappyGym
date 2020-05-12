55ed4b82c681348c5ab9b09e1fe827072fc21f15
kevin flynn <flynny85@gmail.com>
2020-05-12 19:47:49 +0100

    new bg art

--- FlappyGym/Assets/Code/BGSpin.cs
+++ FlappyGym/Assets/Code/BGSpin.cs
@@ -9,63 +9,40 @@ public class BGSpin : MonoBehaviour, ISubscribeState
     [SerializeField]
     private float _speed = 1f;
 
-    private Vector3 _goal;
-    private bool _isTitle = false;
+    private bool _allowRot = false;
 
-    private float _updateTime = 0f;
     private Vector3 _localRot = new Vector3();
 
     void Start()
     {
         ServiceLocator.Resolve<GameState>().SubscribeState(this);
+        _localRot = Vector3.zero;
     }
 
     void Update()
     {
-
         UpdateRotation();
-        UpdateGoal();
     }
 
     private void UpdateRotation()
     {
-        if (_isTitle)
+        if (_allowRot)
         {
-            Vector3 _tmp = this.transform.localEulerAngles;
-            _tmp.y -= Time.deltaTime * 0.5f;
-            this.transform.localEulerAngles = _tmp;
-            return;
+            _localRot.y -= Time.deltaTime * _speed;
+            this.transform.localEulerAngles = _localRot;
         }
-
-        _localRot = Vector3.MoveTowards(_localRot, _goal, Time.deltaTime * _speed);
-        this.transform.localEulerAngles = _localRot;
-    }
-
-
-    private void UpdateGoal()
-    {
-        if (_updateTime > 0f)
-        {
-            _updateTime -= Time.deltaTime;
-            return;
-        }
-
-        _goal.y = ServiceLocator.Resolve<LevelManager>().GetProgress().x * -1f;
-        _updateTime = 0.66f;
     }
 
     public void ReactState(GameStateObj state)
     {
-        _isTitle = (state.state == GameStateObj.gameStates.Title ||
-            state.state == GameStateObj.gameStates.Settings);
+        _allowRot = (state.state == GameStateObj.gameStates.Title ||
+            state.state == GameStateObj.gameStates.Settings ||
+            state.state == GameStateObj.gameStates.Play);
 
         if (state.last == GameStateObj.gameStates.Over &&
-            state.state == GameStateObj.gameStates.Play)
+            state.state != GameStateObj.gameStates.Over)
         {
-            this.transform.localEulerAngles = Vector3.zero;
             _localRot = Vector3.zero;
-            _updateTime = 1f;
-            UpdateGoal();
         }
     }
 }
